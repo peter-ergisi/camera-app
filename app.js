7244e16dac44998c7948658a870aa45505ccf751
peter-ergisi <43963547+peter-ergisi@users.noreply.github.com>
2022-02-14 01:33:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var constraints = { video: { facingMode: "user"}, audio: false};
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
 
+// Define constants
 const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output")
-    cameraSensor = document.querySelector("#camera--sensor")
-    caneraTrigger = document.querySelector("#camera--trigger")
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
 
-
+// Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error){
-        console.error("Oops. Something is broken.", error);
-    });
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
 }
 
 // Take a picture when cameraTrigger is tapped
@@ -25,6 +28,5 @@ cameraTrigger.onclick = function() {
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+    // track.stop();
 };
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
